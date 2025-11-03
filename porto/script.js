0a116c2629d8efc43abaa602dfe7454019a6365a
klnotdemonic <klnotdemonic@gmail.com>
2025-11-03 09:47:21 +0700

    Delete porto directory

--- porto/script.js
+++
@@ -1,8 +0,0 @@
-
-const fullName = "muhammad fharel inti putra racer";
-
-function greet() {
-  alert(`Halo, saya ${fullName}! Selamat datang di portofolio saya.`);
-}
-
-document.getElementById("greetBtn").addEventListener("click", greet);
